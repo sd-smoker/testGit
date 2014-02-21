61fcf3d364670b39c7abc8de0626dc838c15a61d
qsunventuretech <qsun@venturetechasia.net>
2014-02-21 14:23:27 +0800

    Delete user func

--- src/org/qsun/MainTest.java
+++ src/org/qsun/MainTest.java
@@ -16,5 +16,9 @@ public class MainTest
     public void edit(){
         System.out.println("Edit");
     }
+    
+    public void deleteUser(){
+        System.out.println("deleteUser");
+    }
 
 }
